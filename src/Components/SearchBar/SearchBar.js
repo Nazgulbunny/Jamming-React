3df2d50e3ba23c34a4ea080bf9a66df66d3f1fca
Max <maxsermi@gmail.com>
2018-01-17 10:48:40 +0100

    Added a render method to searchbar.js & imported react and exported searchbar

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class SearchBar extends Component{
+render(){
+	<div className="SearchBar">
+	  <input placeholder="Enter A Song, Album, or Artist" />
+	  <a>SEARCH</a>
+	</div>
+	}
+}
+
+export default SearchBar;
+
