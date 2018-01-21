59b5b1f580bc3e82719c77e212614c8f794943f4
Max <maxsermi@gmail.com>
2018-01-21 13:22:23 +0100

    Passed down Playlist to TrackList

--- src/Components/PlayList/PlayList.js
+++ src/Components/PlayList/PlayList.js
@@ -5,9 +5,13 @@ import './PlayList.css';
 class PlayList extends Component{
 	render(){
 		<div className="Playlist">
+		this.playlistName = this.state.playlistName;
+		 this.playlistTracks = this.state.playlistTracks;
+		
 		  <input value="New Playlist"/>
 		  <!-- Add a TrackList component -->
 		  <div className="TrackList">
+		  this.tracks = this.props.playlistTracks;
 		  </div>
 		  <a class="Playlist-save">SAVE TO SPOTIFY</a>
 		</div>
