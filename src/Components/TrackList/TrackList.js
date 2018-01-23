d1c366944e07e77013363245c71d92e6bdcd18a0
Max <maxsermi@gmail.com>
2018-01-23 08:44:38 +0100

    imported track in trackList file

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './TrackList.css';
 import '../Track/Track.js';
-
+import Track from '../Track/Track.js';
 
 class TrackList extends React.Component {
     render() {
         return (
             <div className="TrackList">
                 {this.props.tracks.map(track =>
-                    <track key={track.id} track={track} onRemove={this.props.onRemove} onAdd={this.props.onAdd} shouldAdd={this.props.shouldAdd}/>)}
+             <track key={track.id} track={track} onRemove={this.props.onRemove} onAdd={this.props.onAdd} shouldAdd={this.props.shouldAdd}/>)}
             </div>
         )
     }
